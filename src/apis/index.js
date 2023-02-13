dc66fe47889271410cd89e83defa4c9f82fc0b84
Camilo Cárdenas <camicardenasp@gmail.com>
2023-02-13 16:52:46 -0500

    Added https://tdlb.vercel.app

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://floreslist.herokuapp.com"
+    baseURL: "https://tdlb.vercel.app"
 });
 //http://localhost:3030
 //https://floreslist.herokuapp.com/
